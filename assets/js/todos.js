c1a025af5795d55e5243ebe636a35b7cb8599b41
EstherMx <esmaxi21@gmail.com>
2018-05-23 22:56:12 -0400

    cross out and delete todos

---
+++ assets/js/todos.js
@@ -0,0 +1,10 @@
+//check off specific todos by clicking
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().remove();
+	event.stopPropagation();
+});
